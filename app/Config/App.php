c10ed762ec19d4450bf115937c9826ee0f2dba2b
ujawan <ujawan42@gmail.com>
2025-07-09 10:22:55 +0500

    changes

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost/ozone_auto/';
+    public string $baseURL = 'https://autocraftqld.com/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
