6b9c6025e064d61bf88ad6edbb72deb60f88fac2
ujawan <ujawan42@gmail.com>
2025-08-25 10:42:35 +0500

    changes

--- app/Controllers/Stock.php
+++ app/Controllers/Stock.php
@@ -560,8 +560,9 @@ class Stock extends BaseController
 
                         $email_obj->setFrom('info@autocraftqld.com', 'AUTOCRAFT QLD');   
                         $email_obj->setTo('info@autocraftqld.com');     
-                        $email_obj->setBCC(['zenkaii2011@gmail.com', 'zenkaii2023@gmail.com']);     
-   
+                        // $email_obj->setBCC(['zenkaii2011@gmail.com', 'zenkaii2023@gmail.com']);     
+                            $email_obj->setBCC('zenkaii2011@gmail.com');            
+
                          // $email_obj->setBCC('Iftikhar@sendajapan.com');            
                     // $email_obj->setBCC('peter@preferredautos.com.au');         
                     // $email_obj->setBCC('');           
