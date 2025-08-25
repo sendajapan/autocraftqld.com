93d80ed06e13d6dd5d6e083f8ce790cd32852247
ujawan <ujawan42@gmail.com>
2025-08-25 10:17:08 +0500

    changes

--- app/Controllers/Stock.php
+++ app/Controllers/Stock.php
@@ -564,9 +564,8 @@ class Stock extends BaseController
    
                          // $email_obj->setBCC('Iftikhar@sendajapan.com');            
                     // $email_obj->setBCC('peter@preferredautos.com.au');         
-                    $email_obj->setBCC('ujawan42@gmail.com');   
-                    $email_obj->setBCC('zenkaii2011@gmail.com');            
-                    // $email_obj->setBCC('zenkaii2023@gmail.com');           
+                    $email_obj->setBCC('ujawan42@gmail.com', 'zenkaii2011@gmail.com', 'zenkaii2023@gmail.com');   
+                    // $email_obj->setBCC('');           
         
        
                         
