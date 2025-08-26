9a2bab1059e6fdf2b353b9d4495977336879e95f
Hamza <mhamzah889@gmail.com>
2025-08-26 16:11:48 +0500

    phone number changed

--- app/Views/header.php
+++ app/Views/header.php
@@ -49,7 +49,7 @@
     <div class="top-bar">
         <div class="container d-flex justify-content-end align-items-center">
             <ul class="list-inline mb-0">
-                <li class="list-inline-item"><a href="tel:0418647747" class="text-white text-decoration-none"><i class="fas fa-phone me-2"></i>0418 647 747</a></li>
+                <li class="list-inline-item"><a href="tel:(07) 5537 2900" class="text-white text-decoration-none"><i class="fas fa-phone me-2"></i>(07) 5537 2900</a></li>
                 <li class="list-inline-item"><a href="mailto:cars@preferredautos.com.au" class="text-white text-decoration-none"><i class="fas fa-envelope me-2"></i>cars@preferredautos.com.au</a></li>
             </ul>
         </div>
