6172dc75b1b7b3a987b4d2cebd5b718eec432b30
ujawan <ujawan42@gmail.com>
2025-09-22 10:42:52 +0500

    changes

--- app/Controllers/Stock.php
+++ app/Controllers/Stock.php
@@ -563,14 +563,11 @@ class Stock extends BaseController
                         $email_obj->setFrom('info@autocraftqld.com', 'AUTOCRAFT QLD');   
                         $email_obj->setTo([
                             'info@autocraftqld.com',
-                            'Iftikhar@sendajapan.com',
-                            'mnoman55@gmail.com',
                             'peter@preferredautos.com.au',
-                            'cars@preferredautos.com.au',
-                            'ujawan42@gmail.com'
+                            'cars@preferredautos.com.au'
                         ]);
                         // $email_obj->setTo('info@autocraftqld.com');     
-                        // $email_obj->setBCC(['Iftikhar@sendajapan.com', 'mnoman55@gmail.com', 'peter@preferredautos.com.au', 'cars@preferredautos.com.au']);
+                        $email_obj->setBCC(['Iftikhar@sendajapan.com', 'mnoman55@gmail.com']);
                         $email_obj->setReplyTo('info@autocraftqld.com', 'AUTOCRAFT QLD');
                         
                         // Add headers to improve deliverability
